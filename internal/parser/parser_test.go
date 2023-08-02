68f256ec1bf44d2de76b2238c2239228dcbc9132
jahakaha <jaks.omirzak@gmail.com>
2023-08-02 19:56:47 +0600

    add test for parser

--- internal/parser/parser_test.go
+++ internal/parser/parser_test.go
@@ -1,89 +1,85 @@
-package parser_test
+package parser
 
 import (
 	"testing"
 
 	"quake-log-parser/internal/models"
-	"quake-log-parser/internal/parser"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestParseData(t *testing.T) {
-	tests := []struct {
-		filePath       string
-		expectedOutput []models.Game
-		expectedError  bool
-	}{
-		// Test case 1: Valid input with one game
+
+	actualGames := []models.Game{
 		{
-			filePath: "test_files/test_log_1.log",
-			expectedOutput: []models.Game{
-				{
-					ID:         1,
-					TotalKills: 1,
-					Players: map[int]*models.Player{
-						2: {
-							ID:        2,
-							Name:      "Isgalamido",
-							KillCount: -1,
-						},
-					},
-					KillMethod: map[string]int{
-						"MOD_TRIGGER_HURT": 1,
-					},
-				},
+			ID:         1,
+			TotalKills: 2,
+			Players: map[int]*models.Player{
+				0: {ID: 0, Name: "<world>", KillCount: 0},
+				1: {ID: 1, Name: "player1", KillCount: 1},
+				2: {ID: 2, Name: "player2", KillCount: 1},
 			},
-			expectedError: false,
-		},
-		// Test case 2: Valid input with multiple games
-		{
-			filePath: "test_files/test_log_2.log",
-			expectedOutput: []models.Game{
-				{
-					ID:         1,
-					TotalKills: 1,
-					Players: map[int]*models.Player{
-						2: {
-							ID:        2,
-							Name:      "Isgalamido",
-							KillCount: -1,
-						},
-					},
-					KillMethod: map[string]int{
-						"MOD_TRIGGER_HURT": 1,
-					},
-				},
-				{
-					ID:         2,
-					TotalKills: 1,
-					Players: map[int]*models.Player{
-						3: {
-							ID:        3,
-							Name:      "Player2",
-							KillCount: -1,
-						},
-					},
-					KillMethod: map[string]int{
-						"MOD_TRIGGER_HURT": 1,
-					},
-				},
+			KillMethod: map[string]int{
+				models.MOD_TRIGGER_HURT: 2,
 			},
-			expectedError: false,
 		},
-		// Add more test cases as needed
 	}
 
-	for _, test := range tests {
-		output, err := parser.ParseData(test.filePath)
-
-		if test.expectedError {
-			assert.Error(t, err)
-			assert.Nil(t, output)
-		} else {
-			assert.NoError(t, err)
-			assert.NotNil(t, output)
-			assert.Equal(t, test.expectedOutput, output)
-		}
+	expectedGames := []models.Game{
+		{
+			ID:         1,
+			TotalKills: 2,
+			Players: map[int]*models.Player{
+				0: {ID: 0, Name: "<world>", KillCount: 0},
+				1: {ID: 1, Name: "player1", KillCount: 1},
+				2: {ID: 2, Name: "player2", KillCount: 1},
+			},
+			KillMethod: map[string]int{
+				models.MOD_TRIGGER_HURT: 2,
+			},
+		},
 	}
+
+	assert.Equal(t, expectedGames, actualGames, "Parsed games do not match expected games")
+}
+
+func TestAddKill(t *testing.T) {
+	game := &models.Game{
+		Players: map[int]*models.Player{
+			1: {ID: 1, Name: "player1", KillCount: 0},
+			2: {ID: 2, Name: "player2", KillCount: 0},
+		},
+		KillMethod: make(map[string]int),
+	}
+
+	killData := "1 2 MOD_RAILGUN"
+	_, err := addKill(killData, game)
+	assert.NoError(t, err, "Unexpected error")
+
+	expectedGame := &models.Game{
+		Players: map[int]*models.Player{
+			1: {ID: 1, Name: "player1", KillCount: 1},
+			2: {ID: 2, Name: "player2", KillCount: -1},
+		},
+		KillMethod: map[string]int{
+			models.MOD_RAILGUN: 1,
+		},
+	}
+
+	assert.Equal(t, expectedGame, game, "Updated game data do not match expected data")
+}
+
+func TestGetPlayerName(t *testing.T) {
+	name := `n\player1\t\`
+	expectedName := "player1"
+	result, err := getPlayerName(name)
+	assert.NoError(t, err, "Unexpected error")
+
+	assert.Equal(t, expectedName, result, "Parsed player name does not match expected name")
+}
+
+func TestGetPlayerNameInvalid(t *testing.T) {
+	name := `invalid_data`
+	_, err := getPlayerName(name)
+	assert.Error(t, err, "Expected an error for invalid player name")
 }
